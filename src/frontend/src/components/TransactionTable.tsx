2c098b52580c24fa63f0e1081121c45e6304ee69
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-04-27 01:22:28 -0400

    Fixing archiving and transaction table ui with pagination

--- src/frontend/src/components/TransactionTable.tsx
+++ src/frontend/src/components/TransactionTable.tsx
@@ -1,23 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {
-    TableContainer,
+    Avatar,
+    Card,
+    CardContent, CardHeader,
+    CircularProgress,
+    Paper,
     Table,
-    TableHead,
-    TableRow,
-    TableCell,
     TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
     TablePagination,
-    Paper, CircularProgress, Typography, CardContent, Card,
+    TableRow,
+    Typography,
 } from '@mui/material';
 import {_SERVICE, IcrcTransaction} from "../declarations/token/token.did";
 import {bigIntToDecimalPrettyString} from "../util/bigintutils";
 import {useCanister} from "@connect2ic/react";
-
+import {InfoRounded, SwapHoriz} from "@mui/icons-material";
 
 export default function TransactionTable() {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [transactions, setTransactions] = useState<IcrcTransaction[]>([]);
+    const [transactionAmount, setTransactionAmount] = useState(0);
     const [loading, setLoading] = useState(false);
     const [_tokenActor] = useCanister("token");
     const tokenActor = _tokenActor as unknown as _SERVICE;
@@ -27,8 +33,11 @@ export default function TransactionTable() {
         async function init() {
             const start = BigInt(page) * BigInt(rowsPerPage);
             const length = BigInt(rowsPerPage);
-            const result = await tokenActor.get_transactions({ start, length });
+            const result = await tokenActor.get_transactions({start, length});
+            const tranAmmount = await tokenActor.total_transactions();
+            setTransactionAmount(Number(tranAmmount));
             setTransactions(result.transactions);
+            setRowsPerPage(result.transactions.length);
             setLoading(false);
         }
 
@@ -48,68 +57,89 @@ export default function TransactionTable() {
         rowsPerPage - Math.min(rowsPerPage, transactions.length - page * rowsPerPage);
 
     return (
-        <TableContainer component={Paper} sx={{marginTop: "20px"}}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>From</TableCell>
-                        <TableCell>To</TableCell>
-                        <TableCell>Amount</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {!loading && transactions.length === 0 && (
-                        <TableRow>
-                            <TableCell colSpan={4} align="center">
-                                <Typography variant="subtitle1">No transactions found.</Typography>
-                            </TableCell>
-                        </TableRow>
-                    )}
-                    {!loading && transactions.length > 0 && transactions
-                        .map((transaction) => (
-                            <TableRow key={transaction.timestamp.toString(10)}>
-                                <TableCell>
-                                    {transaction.timestamp.toString(10)}
-                                    {new Date(Number(transaction.timestamp) / 1000000).toLocaleDateString(undefined, {
-                                        year: 'numeric',
-                                        month: 'long',
-                                        day: 'numeric',
-                                        hour: 'numeric',
-                                        minute: 'numeric',
-                                        second: 'numeric',
-                                    })}
-                                </TableCell>
-                                <TableCell>{transaction.from.length > 0 ? transaction.from[0].owner.toText() : ""}</TableCell>
-                                <TableCell>{transaction.args.length > 0 ? transaction.args[0].to.owner.toText() : ""}</TableCell>
-                                <TableCell>{transaction.args.length > 0 ? bigIntToDecimalPrettyString(transaction.args[0].amount) : ""}</TableCell>
-                            </TableRow>
-                        ))}
-                    {emptyRows > 0 && (
-                        <TableRow style={{ height: 53 * emptyRows }}>
-                            <TableCell colSpan={5} />
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={transactions.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
+        <Card sx={{margin: '20px'}}>
+            <CardHeader
+                titleTypographyProps={{ variant: 'h4' }}
+                color={'secondary'}
+                avatar={
+                    <Avatar>
+                        <SwapHoriz />
+                    </Avatar>
+                }
+                title="Transaction History"
             />
+            <CardContent>
+                <TableContainer component={Paper} sx={{marginTop: "20px", height: "640px"}}>
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Date</TableCell>
+                                <TableCell>From</TableCell>
+                                <TableCell>To</TableCell>
+                                <TableCell>Amount</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {!loading && transactions.length === 0 && (
+                                <TableRow>
+                                    <TableCell colSpan={4} align="center">
+                                        <Typography variant="subtitle1">No transactions found.</Typography>
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                            {!loading && transactions.length > 0 && transactions
+                                .map((transaction, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>
+                                            {new Date(Number(transaction.timestamp) / 1000000).toLocaleDateString(undefined, {
+                                                year: 'numeric',
+                                                month: 'long',
+                                                day: 'numeric',
+                                                hour: 'numeric',
+                                                minute: 'numeric',
+                                                second: 'numeric',
+                                            })}
+                                        </TableCell>
+                                        <TableCell>{transaction.from.length > 0 ? transaction.from[0].owner.toText() : ""}</TableCell>
+                                        <TableCell>{transaction.args.length > 0 ? transaction.args[0].to.owner.toText() : ""}</TableCell>
+                                        <TableCell>{transaction.args.length > 0 ? bigIntToDecimalPrettyString(transaction.args[0].amount) : ""}</TableCell>
+                                    </TableRow>
+                                ))}
 
-            {loading && (
-                <Card style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }}>
-                    <CardContent style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection:"column" }}>
-                        <CircularProgress />
-                        <Typography variant="subtitle1" align="center">Loading transactions. This might take a minute.</Typography>
-                    </CardContent>
-                </Card>
-            )}
-        </TableContainer>
+                        </TableBody>
+                    </Table>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={transactionAmount}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+
+                    {loading && (
+                        <Card style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            zIndex: 1
+                        }}>
+                            <CardContent style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                flexDirection: "column"
+                            }}>
+                                <CircularProgress/>
+                                <Typography variant="subtitle1" align="center">Loading transactions. This might take a
+                                    minute.</Typography>
+                            </CardContent>
+                        </Card>
+                    )}
+                </TableContainer>
+            </CardContent>
+        </Card>
     );
 }
--- src/frontend/src/util/josnutil.ts
+++ src/frontend/src/util/josnutil.ts
@@ -1,1 +1,10 @@
+import {hexStringToUint8Array} from "./bigintutils";
+import {enc, SHA256} from "crypto-js";
+
 export const JSON_BIGINT = (error) => JSON.stringify(error, (_, v) => typeof v === 'bigint' ? `${v}n` : v)
+
+
+export function stringToHash(data: string): string {
+    const hashedString = SHA256(data).toString(enc.Hex);
+    return hashedString;
+}
